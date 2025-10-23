b22ec71052fba70a6c7a7f45fa5c848e08c9dc65
Gourango12 <roygourango311@gmail.com>
2025-10-23 20:01:49 +0600

    add header

--- simple-react-project/src/App.jsx
+++ simple-react-project/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 function App() {
   return (
     <div className="avatar">
+
+    <h1>hello Gourango roy</h1>
   <div className="w-24 rounded">
     <img src="https://img.daisyui.com/images/profile/demo/batperson@192.webp" />
   </div>
